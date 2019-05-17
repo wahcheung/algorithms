a9bf9af2c0ce5cd622621f5d9f9b694a424d1c3b
huachang.gong <huachang.gong@outlook.com>
2019-05-17 10:32:53 +0800

    Sort: insertion sort algorithm.

---
+++ sort/insertion_sort.h
@@ -0,0 +1,32 @@
+/**
+ * @file insertion_sort.h
+ * @brief https://www.toptal.com/developers/sorting-algorithms/insertion-sort
+ * @author huachang.gong <huachang.gong@outlook.com>
+ * @version 0.1
+ * @date 2019-05-10
+ */
+#ifndef SORT_INSERTION_SORT_
+#define SORT_INSERTION_SORT_
+
+#include <cstddef>
+#include <utility>
+#include "util/print.h"
+
+namespace algo {
+
+template<typename ValueType>
+void InsertionSort(ValueType nums[], const size_t size) {
+  for (size_t i = 1; i < size; ++i) {
+    ValueType cur = nums[i];
+    size_t j = i;
+    while (j > 0 && cur < nums[j - 1]) {
+      nums[j] = nums[j - 1];
+      --j;
+    }
+    nums[j] = cur;
+  }
+}
+
+}  // namespace algo
+
+#endif  // SORT_INSERTION_SORT_
