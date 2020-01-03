d308b79281b8946c658f3eefc7220219baf3e26b
huachang.gong <huachang.gong@outlook.com>
2020-01-03 17:54:08 +0800

    Util: util for printing vector, change print util implementation.

--- util/print.h
+++ util/print.h
@@ -10,25 +10,38 @@
 
 #include <cstddef>
 #include <iostream>
+#include <vector>
 
 namespace algo {
 namespace common {
 
 /**
- * @brief Print the array in proper format.
+ * @brief Print array in proper format.
  */
 template <typename ValueType>
-void Print(const ValueType array[], const size_t size) {
-  std::cout << "array: ";
-  if (size == 0) {
-    std::cout << "[]" << std::endl;
-    return;
-  }
-  std::cout << "[";
-  for (size_t i = 0; i < size - 1; ++i) {
+void Print(const ValueType array[], const int size) {
+  std::cout << "array: [";
+  for (int i = 0; i < size; ++i) {
     std::cout << array[i] << ", ";
   }
-  std::cout << array[size - 1];
+  if (size != 0) {
+    std::cout << "\b\b";
+  }
+  std::cout << "]" << std::endl;
+}
+
+/**
+ * @brief Print std::vector in proper format.
+ */
+template <typename ValueType>
+void Print(const std::vector<ValueType>& array) {
+  std::cout << "vector: [";
+  for (int i = 0; i < array.size(); ++i) {
+    std::cout << array[i] << ", ";
+  }
+  if (!array.empty()) {
+    std::cout << "\b\b";
+  }
   std::cout << "]" << std::endl;
 }
 
